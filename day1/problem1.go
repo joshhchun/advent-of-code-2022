53bf9a4ace5fd39bf0d45975e41bf990134ecc7a
joshhchun <jchun2@nd.edu>
2022-12-01 09:34:00 -0500

    day1

---
+++ day1/problem1.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+func main() {
+    s := bufio.NewScanner(os.Stdin)
+    nums := make([]int, 0)
+    var num_calories int
+    for s.Scan() {
+        if snack := s.Text(); snack != "" {
+			snack, _ := strconv.Atoi(snack)
+			num_calories += snack
+        } else {
+            nums = append(nums, num_calories)
+            num_calories = 0
+        }
+    }
+
+	max := 0
+	for _, num := range nums {
+		if num > max {
+			max = num
+		}
+	}
+	println(max)
+}
---
+++ day1/problem2.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func main() {
+    s := bufio.NewScanner(os.Stdin)
+    nums := make([]int, 0)
+    var num_calories int
+    for s.Scan() {
+        if snack := s.Text(); snack != "" {
+			snack, _ := strconv.Atoi(snack)
+			num_calories += snack
+        } else {
+            nums = append(nums, num_calories)
+            num_calories = 0
+        }
+    }
+
+    sort.Ints(nums)
+    total := nums[len(nums)-1] + nums[len(nums)-2] + nums[len(nums)-3]
+	println(total)
+}
