7f63e9f83770f4925b2b00895d2da60f6962dc13
joshhchun <jchun2@nd.edu>
2022-12-05 02:18:41 -0500

    day5

---
+++ day5/problem1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+stacks, instr = [l.split('\n') for l in open("input.txt").read().split("\n\n")]
+num_stacks = int(stacks[-1][-2])
+cargo = [[] for _ in range(num_stacks)]
+
+for stack in stacks[::-1]:
+    for i, item in enumerate(stack):
+        if item.isalpha():
+            cargo[(i-1)//4].append(item)
+
+for i in instr:
+    num_move, from_c, to_c = [int(y) for b, y in enumerate(i.split(' ')) if b % 2]
+    cargo[to_c-1].extend([cargo[from_c-1].pop() for _ in range(num_move)])
+
+print(''.join(i.pop() for i in cargo))
---
+++ day5/problem2.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func parseInput() ([]string, []string){
+	file, _ := os.ReadFile("input.txt")
+	parts := strings.Split(string(file), "\n\n")
+	one, two := parts[0], parts[1]
+
+	stacks := strings.Split(one, "\n")
+	instr := strings.Split(two, "\n")
+	return stacks, instr
+
+}
+
+func main() {
+	stacks, instr := parseInput()
+	numStacks, _ := strconv.Atoi(string(stacks[len(stacks)-1][len(stacks[len(stacks)-1])-2]))
+	cargo := make([][]string, numStacks)
+
+
+	for i := len(stacks) - 1; i >= 0; i-- {
+		for j, item := range stacks[i] {
+			if item >= 'A' && item <= 'Z' {
+				cargo[(j-1)/4] = append(cargo[(j-1)/4], string(item))
+			}
+		}
+	}
+
+	for _, line := range instr {
+		parts := strings.Split(line, " ")
+		numMove, _ := strconv.Atoi(parts[1])
+		fromC, _ := strconv.Atoi(parts[3])
+		toC, _ := strconv.Atoi(parts[5])
+		cargo[toC-1] = append(cargo[toC-1], cargo[fromC-1][len(cargo[fromC-1])-numMove:]...)
+		cargo[fromC-1] = cargo[fromC-1][:len(cargo[fromC-1])-numMove]
+	}
+	
+	var result string
+	for _, stack := range cargo {
+		result += stack[len(stack)-1]
+	}
+	fmt.Println(result)
+}
