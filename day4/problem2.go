3085f3c223943d36c08966b36a5e8177b9e493e3
joshhchun <jchun2@nd.edu>
2022-12-05 02:26:38 -0500

    day4: fix formatting + add time.txt

--- day4/problem2.go
+++ day4/problem2.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
+    "fmt"
+    "os"
+    "strconv"
+    "strings"
 )
 
 func main() {
-	file, _ := os.ReadFile("input.txt")
+    file, _ := os.ReadFile("input.txt")
     lines := string(file)
-	count := 0
+    count := 0
 
     for _, line := range strings.Split(lines, "\n") {
-		x := strings.Split(line, ",")
-		x0 := strings.Split(x[0], "-")
-		x1 := strings.Split(x[1], "-")
+        x := strings.Split(line, ",")
+        x0 := strings.Split(x[0], "-")
+        x1 := strings.Split(x[1], "-")
 
-		var pair [2][2]int
-		pair[0][0], _ = strconv.Atoi(x0[0])
-		pair[0][1], _ = strconv.Atoi(x0[1])
-		pair[1][0], _ = strconv.Atoi(x1[0])
-		pair[1][1], _ = strconv.Atoi(x1[1])
+        var pair [2][2]int
+        pair[0][0], _ = strconv.Atoi(x0[0])
+        pair[0][1], _ = strconv.Atoi(x0[1])
+        pair[1][0], _ = strconv.Atoi(x1[0])
+        pair[1][1], _ = strconv.Atoi(x1[1])
 
-		if pair[0][0] > pair[1][0] {
-			pair[0], pair[1] = pair[1], pair[0]
-		}
-		if pair[0][1] >= pair[1][0] {
-			count++
-		}
-	}
+        if pair[0][0] > pair[1][0] {
+            pair[0], pair[1] = pair[1], pair[0]
+        }
+        if pair[0][1] >= pair[1][0] {
+            count++
+        }
+    }
 
-	fmt.Println(count)
+    fmt.Println(count)
 }
